b3c76fe4189670a2644cd713782b1a501f3c6adb
AndreaTurco <turcoandrea85@gmail.com>
2014-09-09 01:09:55 +0200

    adesso funzionano i controller

--- app/controllers/worksExploreController.js
+++
@@ -1,9 +0,0 @@
-app.controller('worksExploreController', ['$scope', '$http',
-	function($scope,$http){
-		$http.get('json/works/works.json').success( function(data){
-    		$scope.allWorks = data;
-    	});
-
-    	$scope.orderProp = 'id';
-	}
-]);
